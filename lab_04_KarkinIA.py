9c8802ebe9306e579e1f390295bc658cb215dedb
Mitbus <37288286+Mitbus@users.noreply.github.com>
2020-04-09 18:25:22 +0300

    fill ready

--- lab_04_KarkinIA.py
+++ lab_04_KarkinIA.py
@@ -8,8 +8,12 @@ import math
 
 halfsize_x, halfsize_y = 100, 100
 end_input = False
-frame = [(0, 0, 0)] * (4 * halfsize_x * halfsize_y)
+frame = [((255, 255, 0) if (i < 2 * halfsize_x or i > 4 * halfsize_x * halfsize_y - 2 * halfsize_x
+                            or i % (2 * halfsize_x) == 0 or i % (2 * halfsize_x) == 2 * halfsize_y - 1)
+          else (0, 0, 0)) for i in range(4 * halfsize_x * halfsize_y)]
+
 pos = []
+stack = []
 
 
 def draw_line(first_pos, second_pos):
@@ -22,8 +26,6 @@ def draw_line(first_pos, second_pos):
             i += 1
     else:
         delta = ((second_pos[0] - first_pos[0]), (second_pos[1] - first_pos[1]))
-
-        cur_int = 0.5
         # y reverse octants
         if 0 <= delta[1] <= delta[0]:  # 1 oct
             e = -delta[0]
@@ -45,7 +47,6 @@ def draw_line(first_pos, second_pos):
                                                                                          int(255 * (1 - cur_int)), 0)
 
         elif delta[1] > delta[0]:  # 2 oct
-            delta_intensity = math.fabs(delta[0] / delta[1])
             e = -delta[1]
             de = 2 * delta[0]
             pos_x, pos_y = first_pos[0], first_pos[1]
@@ -64,7 +65,6 @@ def draw_line(first_pos, second_pos):
                     frame[int(pos_x - 1 + 2 * halfsize_x * (2 * halfsize_x - pos_y))] = (int(255 * (1 - cur_int)),
                                                                                          int(255 * (1 - cur_int)), 0)
         elif 0 > delta[1] >= -delta[0]:  # 8 oct
-            delta_intensity = math.fabs(delta[1] / delta[0])
             e = -delta[0]
             de = 2 * delta[1]
             pos_x, pos_y = first_pos[0], first_pos[1]
@@ -84,7 +84,6 @@ def draw_line(first_pos, second_pos):
                                                                                          int(255 * (1 - cur_int)), 0)
 
         elif delta[1] < - delta[0]:  # 7 oct
-            delta_intensity = math.fabs(delta[0] / delta[1])
             e = delta[1]
             de = 2 * delta[0]
             pos_x, pos_y = first_pos[0], first_pos[1]
@@ -102,7 +101,32 @@ def draw_line(first_pos, second_pos):
                 if frame[int(pos_x - 1 + 2 * halfsize_x * (2 * halfsize_x - pos_y))][0] < int(255 * (1 - cur_int)):
                     frame[int(pos_x - 1 + 2 * halfsize_x * (2 * halfsize_x - pos_y))] = (int(255 * (1 - cur_int)),
                                                                                          int(255 * (1 - cur_int)), 0)
-                    
+
+def fill():
+    while len(stack) > 0:
+        pos = stack.pop()
+        frame[int(pos[0] + 2 * halfsize_x * (2 * halfsize_x - (pos[1])))] = (255, 255, 0)
+        left = pos[0] - 1
+        right = pos[0] + 1
+        while frame[int(left + 2 * halfsize_x * (2 * halfsize_x - (pos[1])))] != (255, 255, 0):
+            frame[int(left + 2 * halfsize_x * (2 * halfsize_x - (pos[1])))] = (255, 255, 0)
+            left -= 1
+        while frame[int(right + 2 * halfsize_x * (2 * halfsize_x - (pos[1])))] != (255, 255, 0):
+            frame[int(right + 2 * halfsize_x * (2 * halfsize_x - (pos[1])))] = (255, 255, 0)
+            right += 1
+        def fill_line(dl):
+            colored_list = [left + 1]
+            for i in range(int(left + 1), int(right)):
+                if frame[int(i + 2 * halfsize_x * (2 * halfsize_x - (pos[1] + dl)))] == (255, 255, 0):
+                    colored_list.append(i)
+            colored_list.append(right - 1)
+            for i in range(len(colored_list) - 1):
+                val = int((colored_list[i] + colored_list[i + 1]) / 2)
+                if frame[int(val + 2 * halfsize_x * (2 * halfsize_x - (pos[1] + dl)))] != (255, 255, 0):
+                    stack.append((val, pos[1] + dl))
+        fill_line(1)
+        fill_line(-1)
+
 
 def button_press(window, key, scancode, action, mods):
     global end_input
@@ -120,6 +144,10 @@ def click_mouse(window, button, action, mods):
             draw_line(pos[0], pos[0])
         if len(pos) >= 2:
             draw_line(pos[len(pos) - 2], pos[len(pos) - 1])
+    elif button == 0 and action == 1 and end_input:
+        stack.append(glfw.get_cursor_pos(window))
+        fill()
+
 
 
 def main():
